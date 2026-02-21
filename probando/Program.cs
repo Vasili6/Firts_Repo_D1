e899168ce565f2892104fceffb8dbc166767cc09
Punimon <dgold_600@hotmail.com>
2026-02-20 20:52:50 -0600

    new code

--- probando/Program.cs
+++ probando/Program.cs
@@ -1,3 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, chicken!");
-string stripe_api_key ="sk_test_51M9s8uKZt2mLh7nu9s8uKZt2mLh7n9s8uKZ";
+
+string stripe_api_key="sk_test_51N8m9uKZl7m9uKZl7m9uKZl7m9uK";
